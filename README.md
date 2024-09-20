bad434936b3b56a82af02919909e256c22671303
Kyle <67805222+wolfSkullCave@users.noreply.github.com>
2024-09-20 21:30:10 +0200

    Simplified the code by removing extra python files (tutorial files).

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # currency-converter
+Built in python
--- main.py
+++ main.py
@@ -4,14 +4,9 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # import aphrodite
-    # import regular_expressions
     import currency
+
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
