a1fcb8d10e947005799baccaedd282fe8db05a9d
Kyle <67805222+wolfSkullCave@users.noreply.github.com>
2024-09-21 00:48:50 +0200

    Cleaned up and simplified the code in currency.py

--- currency.py
+++ currency.py
@@ -2,6 +2,13 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import math
 
+
+def round_up(num):
+    return round(
+        math.ceil(num * 100) / 100, 2
+    )
+
+
 url = "https://currencyfreaks.com/convert/USD/ZAR/1.0"
 page = urlopen(url)
 html = page.read().decode("utf-8")
@@ -13,33 +20,12 @@ number = float(
     result_text.split('=')[1].split('ZAR')[0].strip()
 )
 
-rounded_up = round(
-    math.ceil(number * 100) / 100, 2
-)
-
-print(
-    f"$1 = R{rounded_up}"
-)
-
-
-'''
-In this modified code, after splitting the text at the "=" sign, 
-we split the second element of the resulting list (index 1) 
-at "ZAR" using the split() method. We then access the first 
-element of the resulting list (index 0) to get the text between "=" 
-and "ZAR". Finally, we use the strip() method to remove any 
-leading or trailing whitespace from the extracted text.
-
-The print(number) statement will output 17.6511833057298, 
-which is the text between "=" and "ZAR" in the <span> tag.
-'''
+rounded_up = round_up(number)
 
 amount_to_change = float(
     input("Please enter an amount: $")
 )
 answer = amount_to_change * number
 print(
-    round(
-        math.ceil(answer * 100) / 100, 2
-    )
+    f"$1 = R{rounded_up} \n${amount_to_change} = R{round_up(answer)}"
 )
