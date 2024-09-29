0721e352c9b6ea8ca83093f466255422119efa0a
Kyle <67805222+wolfSkullCave@users.noreply.github.com>
2024-09-29 17:29:27 +0200

    removed all files not related to the project

--- main.py
+++ main.py
@@ -3,15 +3,10 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def hello(): {
-    print("hello world")
-}
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     import currency
-
-    hello()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
