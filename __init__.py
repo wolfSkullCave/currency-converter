94089730ccef5796951e2a25c85c20a4f0958c21
wolf_skullcave-Laptop <7805222+wolfSkullCave@users.noreply.github.com>
2025-01-07 15:14:17 +0200

    Reorganised files and folders again. This time more simplified.

---
+++ __init__.py
@@ -0,0 +1,4 @@
+"""An application to convert Dollars(USD) into Rands(ZAR)"""
+
+__version__ = "1.0.0-alpha"
+__description__ = "A simple currency converter that uses web scraping to retrieve the exchange rate."
---
+++ currency.py
@@ -0,0 +1,46 @@
+""" Convert Dollars(USD) into Rands(ZAR).
+
+This module contains functions for currency conversion.
+
+The conversion is done by scraping the website: https://currencyfreaks.com/
+
+The conversion rate is rounded up to the nearest cent.
+
+The functions are:
+- round_up(num): rounds up the given number to the nearest cent.
+- main(): scrapes the website and prints out the conversion of $1 and a
+specified amount to Rand."""
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import math
+
+
+def round_up(num):
+    return round(
+        math.ceil(num * 100) / 100, 2
+    )
+
+__version__ = "1.0.0a0"
+__description__ = "Convert Dollars(USD) into Rands(ZAR)"
+
+url = "https://currencyfreaks.com/convert/USD/ZAR/1.0"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
+
+div_result = soup.find("div", class_='result')
+result_text = div_result.text
+number = float(
+    result_text.split('=')[1].split('ZAR')[0].strip()
+)
+
+rounded_up = round_up(number)
+
+amount_to_change = float(
+    input("Please enter an amount: $")
+)
+answer = amount_to_change * number
+print(
+    f"$1 = R {rounded_up} \n${amount_to_change} = R {round_up(answer)}"
+)
