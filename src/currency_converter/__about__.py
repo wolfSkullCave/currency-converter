0e5d7176582e6883035a8a93092f65b44ab8269f
wolf_skullcave-Laptop <7805222+wolfSkullCave@users.noreply.github.com>
2025-01-11 20:48:36 +0200

    ready for testpypi

--- src/currency_converter/__about__.py
+++ src/currency_converter/__about__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2025-present wolf_skullcave-Laptop <7805222+wolfSkullCave@users.noreply.github.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.0.1a0"
+__version__ = "0.0.1a3"
--- src/currency_converter/__init__.py
+++ src/currency_converter/__init__.py
@@ -1,3 +1,5 @@
 # SPDX-FileCopyrightText: 2025-present wolf_skullcave-Laptop <7805222+wolfSkullCave@users.noreply.github.com>
 #
 # SPDX-License-Identifier: MIT
+
+from .__about__ import __version__
